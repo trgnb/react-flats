cb578b1733d0af1eb8243dcdb8e4d9e4b24a9c59
Belis Turegun <trgnb6@gmail.com>
2020-04-26 21:46:56 -0400

    added components

---
+++ src/components/app.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import GoogleMapReact from 'google-map-react';
+
+import FlatList from './flat_list';
+import Marker from './marker';
+
+import flats from '../../data/flats';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      flats,
+      selectedFlat: flats[0]
+    };
+  }
+
+  selectFlat = (id) => {
+    this.setState({
+      selectedFlat: flats[id]
+    });
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
